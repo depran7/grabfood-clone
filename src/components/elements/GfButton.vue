5375b881bd982abd22eb1972d6e250822e860a74
ade pranaya 153040050 <ade.pranaya@mail.unpas.ac.id>
2022-02-27 17:03:41 +0700

    fix border width grab food button

--- src/components/elements/GfButton.vue
+++ src/components/elements/GfButton.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="py-2 px-3 border-2 border-gray-100 rounded flex items-center justify-center"
+    class="py-2 px-3 border border-gray-100 rounded flex items-center justify-center"
   >
     <slot>Button</slot>
   </button>
