dcc82c0e4b59c61a4ee44e94c394cd3a0cc0ab00
ade pranaya 153040050 <ade.pranaya@mail.unpas.ac.id>
2022-02-27 09:59:46 +0700

    add elements search location

---
+++ src/components/elements/SearchLocation.vue
@@ -0,0 +1,13 @@
+<script setup>
+import IconLocation from "@/components/icons/IconLocation.vue";
+
+import GfButton from "@/components/elements/GfButton.vue";
+</script>
+<template>
+  <GfButton
+    class="w-full mx-auto rounded-lg !border-1 !border-gray-200 gap-2 py-1"
+  >
+    <IconLocation />
+    <span class="text-gray-400/70">Type your location</span>
+  </GfButton>
+</template>
