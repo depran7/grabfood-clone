1d6b640c38b8a033ab5755a2b30211472e9f5acd
ade pranaya 153040050 <ade.pranaya@mail.unpas.ac.id>
2022-02-27 17:04:33 +0700

    change border color

--- src/components/elements/SearchLocation.vue
+++ src/components/elements/SearchLocation.vue
@@ -5,7 +5,7 @@ import GfButton from "@/components/elements/GfButton.vue";
 </script>
 <template>
   <GfButton
-    class="w-full mx-auto rounded-lg !border-1 !border-gray-200 gap-2 py-1"
+    class="w-full mx-auto rounded-lg !border-1 !border-gray-300 gap-2 py-1"
   >
     <IconLocation />
     <span class="text-gray-400/70">Type your location</span>
