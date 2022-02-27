5f2853b63beda0c3d0161b71e0ab3ca2a2a044a4
ade pranaya 153040050 <ade.pranaya@mail.unpas.ac.id>
2022-02-27 10:07:57 +0700

    add breadchrumb component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,29 @@
 <script setup lang="ts">
 import Header from "@/components/parts/Header.vue";
+import Breadcrumb from "@/components/elements/Breadcrumb.vue";
+
+import { ref } from "vue";
+
+const breadcrumbs = ref([
+  {
+    name: "Home",
+    url: "/",
+  },
+  {
+    name: "Restaurant",
+    url: "/restaurants",
+  },
+  {
+    name: "Do Yan Seafood - Sumbersekar",
+  },
+]);
 </script>
 
 <template>
   <Header></Header>
   <main>
-    <h1>hello world</h1>
+    <div class="max-w-7xl mx-auto px-8 mt-12">
+      <Breadcrumb :items="breadcrumbs"></Breadcrumb>
+    </div>
   </main>
 </template>
---
+++ src/components/elements/Breadcrumb.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+interface BreadcrumbItem {
+  name: string;
+  url?: string;
+}
+defineProps<{
+  items: BreadcrumbItem[];
+}>();
+</script>
+
+<template>
+  <ul class="flex gap-2">
+    <li
+      v-for="(item, index) in items"
+      :key="index"
+      class="flex items-center gap-2"
+    >
+      <template v-if="index < items.length - 1">
+        <a
+          :href="item.url"
+          class="text-curious-blue hover:text-primary transition-colors"
+          >{{ item.name }}
+        </a>
+        <!-- divider -->
+        <span v-if="index < items.length - 1"
+          ><img src="@/assets/images/icons/icon-next.svg" alt="icon next"
+        /></span>
+      </template>
+      <template v-else>
+        <span>{{ item.name }}</span>
+      </template>
+    </li>
+  </ul>
+</template>
