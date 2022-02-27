b97e3b0c7aeadae0cc9c8fbff38be0636377e22d
ade pranaya 153040050 <ade.pranaya@mail.unpas.ac.id>
2022-02-27 09:49:29 +0700

    add Header Component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Header from "@/components/parts/Header.vue";
+</script>
 
 <template>
+  <Header></Header>
   <main>
     <h1>hello world</h1>
   </main>
---
+++ src/components/parts/Header.vue
@@ -0,0 +1,60 @@
+<script setup>
+import Logo from "@/components/icons/IconLogo.vue";
+import GfButton from "@/components/elements/GfButton.vue";
+</script>
+
+<template>
+  <header class="sticky w-full z-20 top-0 bg-white">
+    <div class="max-w-7xl h-20 mx-auto px-8 flex justify-between items-center">
+      <Logo class="fill-primary logo"></Logo>
+      <div class="hidden lg:flex flex-1">
+        <div class="w-1/2">
+          <GfButton
+            class="w-5/6 mx-auto rounded-lg !border-1 !border-gray-200 gap-2"
+          >
+            <img
+              src="@/assets/images/icons/icon-cart-default-black.svg"
+              alt=""
+              class="h-4"
+            />
+            <span class="text-gray-400/70">Type your location</span>
+          </GfButton>
+        </div>
+      </div>
+      <div class="flex gap-2">
+        <GfButton>
+          <img
+            src="@/assets/images/icons/icon-cart-default-black.svg"
+            alt=""
+            class="h-4"
+          />
+        </GfButton>
+        <GfButton class="text-sm font-semibold text-gray-500">
+          Login/Sign up
+        </GfButton>
+      </div>
+    </div>
+    <div
+      class="max-w-7xl h-20 mx-auto px-8 flex justify-between items-center lg:hidden"
+    >
+      <div class="w-full">
+        <GfButton
+          class="w-full mx-auto rounded-lg !border-1 !border-gray-200 gap-2"
+        >
+          <img
+            src="@/assets/images/icons/icon-cart-default-black.svg"
+            alt=""
+            class="h-4"
+          />
+          <span class="text-gray-400/70">Type your location</span>
+        </GfButton>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.logo {
+  height: 50px;
+}
+</style>
